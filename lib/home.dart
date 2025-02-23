c4a773a84535c18ad2d46d6d66a2bcd0940f7adb
Swarnaly Sarker <sarker15-5943@diu.edu.bd>
2025-02-23 11:48:08 +0600

    done

---
+++ lib/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text('This is the First Screen'),
+      ),
+    );
+  }
+}
